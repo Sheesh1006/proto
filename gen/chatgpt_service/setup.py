9d1d3deb63a7489b0e0f6b755524940341fdbaa8
dimicorn <zagorulia.ds@phystech.edu>
2025-04-07 21:39:19 +0300

    init setup.py

---
+++ gen/chatgpt_service/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='chatgpt_service',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'grpcio',
+        'grpcio-tools'
+    ],
+    description='A package for ChatGPT service',
+    author='Sheesh1006',
+    author_email='cloudmeme@yandex.ru',
+    url='https://github.com/Sheesh1006/service-chatgpt',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+)
